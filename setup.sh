c80d1af70ef834f236c2e628e514cb13c5f6a34b
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-04-30 21:03:28 +0900

    Add an environment setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Install Python dependencies
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+conda install -c pyg pytorch-sparse
+conda install -c pyg pytorch-scatter
+conda install -c pyg pytorch-cluster
+conda install -c pyg pyg
+pip install ogb
+conda install -c dglteam/label/cu113 dgl
+pip install yacs
+pip install transformers
+pip install --upgrade accelerate
+
+# For typing
+pip install pandas-stubs
