b0568d5aa608ef94da5c0934b4ec1121f10c9811
Noppanat Wadlom <noppanat.wad@gmail.com>
2024-05-16 13:21:47 +0900

    Add script for running all datasets

--- run_cas_optuna.sh
+++ run_cas_optuna.sh
@@ -1,15 +1,17 @@
-datasets=("arxiv_2023")
+datasets=("ogbn-arxiv" "cora" "pubmed")
 results="results"
 
-rm -r $results
-mkdir $results
+mkdir -p $results
 
 for dataset in "${datasets[@]}"
 do
-    python -m core.runCaS_optuna dataset $dataset gnn.model.name MLP seed 0 cas.optuna.n_jobs 4 >> "${results}/${dataset}_mlp.txt" 2>> "${results}/${dataset}_mlp.err" &
-    python -m core.runCaS_optuna dataset $dataset gnn.model.name GCN seed 0 cas.optuna.n_jobs 4 >> "${results}/${dataset}_gcn.txt" 2>> "${results}/${dataset}_gcn.err" &
-    python -m core.runCaS_optuna dataset $dataset gnn.model.name SAGE seed 0 cas.optuna.n_jobs 4 >> "${results}/${dataset}_sage.txt" 2>> "${results}/${dataset}_sage.err" &
-    python -m core.runCaS_optuna dataset $dataset gnn.model.name RevGAT seed 0 cas.optuna.n_jobs 4 >> "${results}/${dataset}_revgat.txt" 2>> "${results}/${dataset}_revgat.err" &
+    dataset_results="${results}/${dataset}"
+    rm -r $dataset_results
+    mkdir -p $dataset_results
+    python -m core.runCaS_optuna dataset $dataset gnn.model.name MLP seed 0 cas.optuna.n_jobs 4 >> "${dataset_results}/${dataset}_mlp.txt" 2>> "${dataset_results}/${dataset}_mlp.err" &
+    python -m core.runCaS_optuna dataset $dataset gnn.model.name GCN seed 0 cas.optuna.n_jobs 4 >> "${dataset_results}/${dataset}_gcn.txt" 2>> "${dataset_results}/${dataset}_gcn.err" &
+    python -m core.runCaS_optuna dataset $dataset gnn.model.name SAGE seed 0 cas.optuna.n_jobs 4 >> "${dataset_results}/${dataset}_sage.txt" 2>> "${dataset_results}/${dataset}_sage.err" &
+    python -m core.runCaS_optuna dataset $dataset gnn.model.name RevGAT seed 0 cas.optuna.n_jobs 4 >> "${dataset_results}/${dataset}_revgat.txt" 2>> "${dataset_results}/${dataset}_revgat.err" &
+    wait
+    echo "DONE: $dataset"
 done
-
-wait
